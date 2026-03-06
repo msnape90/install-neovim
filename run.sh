58284185a3f2407a73fcfd58da76fa695b29711e
msnape90 <msnape90@gmail.com>
2026-03-06 15:02:30 -0500

    added apt install

--- run.sh
+++ run.sh
@@ -12,10 +12,8 @@ fi
 echo "Success: Running with sudo. Original user: $SUDO_USER"
 
 # update and install curent available apt packages
-sudo apt update -y && sudo apt install
-# first deps
-git curl wget
-# build deps
-ninja-build gettext cmake unzip build-essential libtool libtool-bin autoconf automake pkg-config doxygen
-# neovim deps
-ripgrep fd-find xclip python3-pip python3-venv gdb lldb gcc ncurses-term sqlite3 libsqlite3-dev fzf luarocks lazygit imagemagick
+sudo apt update -y && sudo apt install git curl wget ninja-build \
+  gettext cmake unzip build-essential libtool libtool-bin autoconf \
+  automake pkg-config doxygen ripgrep fd-find xclip python3-pip \
+  python3-venv gdb lldb gcc ncurses-term sqlite3 libsqlite3-dev \
+  fzf luarocks lazygit imagemagick

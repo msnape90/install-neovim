f1f3072db1343c4c405c2b48e3a7124bd7e3fb8f
msnape90 <msnape90@gmail.com>
2026-03-06 14:58:23 -0500

    added apt install

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "starting Neovim Install"
+
+# Check if SUDO_USER is empty (not run with sudo)
+if [ -z "$SUDO_USER" ]; then
+  echo "Error: This script must be run with sudo. Use: sudo ./script.sh" >&2
+  exit 1
+fi
+
+# If we reach here, run with sudo
+echo "Success: Running with sudo. Original user: $SUDO_USER"
+
+# update and install curent available apt packages
+sudo apt update -y && sudo apt install
+# first deps
+git curl wget
+# build deps
+ninja-build gettext cmake unzip build-essential libtool libtool-bin autoconf automake pkg-config doxygen
+# neovim deps
+ripgrep fd-find xclip python3-pip python3-venv gdb lldb gcc ncurses-term sqlite3 libsqlite3-dev fzf luarocks lazygit imagemagick

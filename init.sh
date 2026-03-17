b2140a20cc730ffbd471ce2aeac4efaa5298f793
msnape90 <msnape90@gmail.com>
2026-03-17 15:16:21 -0400

    added init file

--- init.sh
+++ init.sh
@@ -12,7 +12,20 @@ sudo -n true 2>/dev/null || {
 
 sudo apt update && sudo apt install netselect-apt -y
 rmdir /tmp/aptsources >/dev/null 2>&1
-mkdir -p /tmp/sources >/dev/null 2>&1
-pushd /tmp/sources
-sudo netselect-apt
+mkdir -p /tmp/aptsources >/dev/null 2>&1
+pushd /tmp/aptsources
+echo "searching for fastest debian apt repo"
+sudo netselect-apt >/dev/null 2>&1 && "fastest deb repo found"
 popd >/dev/null 2>&1
+
+# find the fist occurance (1st line) of a file the conatins string then awk the second column dilimited by space
+OLDDEBSOURCE=$(grep "http" /etc/apt/sources.list -m 1 | awk '{print $2}')
+NEWDEBSOURCE=$(grep "http" /tmp/aptsources/sources.list -m 1 | awk '{print $2}')
+
+# backup current sources file
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+# replace the first occurance of the old deb source with the new one
+sudo sed -iz "s#$OLDDEBSOURCE#$NEWDEBSOURCE#" /etc/apt/sources.list
+
+echo "updated /etc/apt/sources.list to the fastest repo. if error occurs restore from /etc/apt/sources.list.bak"

15cbb66a7c5d126af998b9730e2cd92d4df124f0
msnape90 <msnape90@gmail.com>
2026-03-17 14:22:58 -0400

    added init file

--- init.sh
+++ init.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# attempts to gain sudo access if not already accquired
+
+sudo -v
+
+# exits the script if sudo access is not granted
+sudo -n true 2>/dev/null || {
+  echo "You must have the ability to run sudo commands to execute this script"
+  exit 1
+}
+
 sudo apt update && sudo apt install netselect-apt -y
 rmdir /tmp/aptsources >/dev/null 2>&1
 mkdir -p /tmp/sources >/dev/null 2>&1
 pushd /tmp/sources
-netselect-apt
+sudo netselect-apt
 popd >/dev/null 2>&1

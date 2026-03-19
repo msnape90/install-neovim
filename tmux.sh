3c1fece5f03a6bb6f347072c327e5972310c97af
msnape90 <msnape90@gmail.com>
2026-03-19 16:20:17 -0400

    changed tmux install

--- tmux.sh
+++ tmux.sh
@@ -10,11 +10,21 @@ sudo -n true 2>/dev/null || {
   exit 1
 }
 
+CONFIG_DIR="$HOME/.config/tmux"
+TMUX_CONF="$CONFIG_DIR/tmux.conf"
+TPM_DIR="$CONFIG_DIR/plugins/tpm"
+
 sudo apt update && sudo apt install tmux git
 
-CONFIGDIR="$HOME/.config/tmux"
-
 mkdir -p "$CONFIGDIR"
-touch "$CONFIGDIR/tmux.conf"
 
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "$TPM_DIR"]; then
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+fi
+
+mv ./.config/tmux/reload-tmux.conf "$CONFIG_DIR/reload-tmux.conf"
+mv ./.config/tmux/tmux.conf "$TMUX_CONF"
+
+tmux -f "$TMUX_CONF" new-session -d -s bootstrap 'sleep 1'
+tmux -f "$TMUX_CONF" run-shell "$TPM_DIR/bin/install_plugins"
+tmux kill-session -t bootstrap

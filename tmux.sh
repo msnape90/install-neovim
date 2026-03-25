0bee50fdf24e1d1a6c9fb357c86c61e1b12e4c1e
msnape90 <msnape90@gmail.com>
2026-03-25 13:48:40 -0400

    changed vars

--- tmux.sh
+++ tmux.sh
@@ -12,49 +12,42 @@ sudo -n true 2>/dev/null || {
 
 # base dirs
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG_DIR="$HOME/.config/tmux"
-sys_dir_config="$HOME/.config"
-script_dir_config="$SCRIPT_DIR/.config"
+SYS_DIR_CONFIG="$HOME/.config"
+SCRIPT_DIR_CONFIG="$SCRIPT_DIR/.config"
 
 # script dirs
-TMUX_DIR="$SCRIPT_DIR/.config/tmux"
-script_dir_tmux="$script_dir_config/tmux"
+SCRIPT_DIR_TMUX="$SCRIPT_DIR_CONFIG/tmux"
 
 # system dirs
-TPM_DIR="$CONFIG_DIR/plugins/tpm"
-sys_dir_tpm="$sys_dir_config/tmux/plugins/tpm"
-sys_dir_tmux="$sys_dir_config/tmux"
+SYS_DIR_TPM="$SYS_DIR_CONFIG/tmux/plugins/tpm"
+SYS_DIR_TMUX="$SYS_DIR_CONFIG/tmux"
 # RESURRECT_DIR="$HOME/.local/.tmux/resurrect"
 
 # script file locations
-CONFIG_FILE="$TMUX_DIR/tmux.conf"
-RELOAD_FILE="$TMUX_DIR/reload-tmux.conf"
-script_file_tmux_conf="$script_dir_tmux/tmux.conf"
-script_file_reload_tmux_conf="$script_dir_tmux/reload-tmux.conf"
+SCRIPT_FILE_TMUX_CONF="$SCRIPT_DIR_TMUX/tmux.conf"
+SCRIPT_FILE_RELOAD_TMUX_CONF="$SCRIPT_DIR_TMUX/reload-tmux.conf"
 
 # system file locations
-LOC_TMUX_FILE="$CONFIG_DIR/tmux.conf"
-LOC_RELOAD_FILE="$CONFIG_DIR/reload-tmux.conf"
-sys_file_tmux_conf="$sys_dir_tmux/tmux.conf"
-sys_file_reload_tmux_conf="$sys_dir_tmux/reload-tmux.conf"
-sys_file_tpm_install_plugins="$sys_dir_tpm/bin/install_plugins"
+SYS_FILE_TMUX_CONF="$SYS_DIR_TMUX/tmux.conf"
+SYS_FILE_RELOAD_TMUX_CONF="$SYS_DIR_TMUX/reload-tmux.conf"
+SYS_FILE_TPM_INSTALL_PLUGINS="$SYS_DIR_TPM/bin/install_plugins"
 
-rm -rf "$sys_dir_tmux"
-mkdir -p "$sys_dir_tmux"
+rm -rf "$SYS_DIR_TMUX"
+mkdir -p "$SYS_DIR_TMUX"
 # mkdir -p "$RESURRECT_DIR"
 
 sudo apt update && sudo apt install fzf tmux git -y
 
-if [ ! -d "$sys_dir_tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+if [ ! -d "$SYS_DIR_TPM" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$SYS_DIR_TPM"
 fi
 
-cp "$script_file_tmux_conf" "$sys_file_tmux_conf"
-cp "$script_file_reload_tmux_conf" "$sys_file_reload_tmux_conf"
+cp "$SCRIPT_FILE_TMUX_CONF" "$sys_file_tmux_conf"
+cp "$SCRIPT_FILE_RELOAD_TMUX_CONF" "$sys_file_reload_tmux_conf"
 
-tmux -f "$sys_file_tmux_conf" new-session -d -s bootstrap
+tmux -f "$SYS_FILE_TMUX_CONF" new-session -d -s bootstrap
 tmux send-keys 'sleep 1' C-m &&
   sleep 1
-tmux -f "$sys_file_tmux_conf" run-shell "$sys_file_tpm_install_plugins" &&
+tmux -f "$SYS_FILE_TMUX_CONF" run-shell "$SYS_FILE_TPM_INSTALL_PLUGINS" &&
   sleep 3
 tmux kill-session -t bootstrap

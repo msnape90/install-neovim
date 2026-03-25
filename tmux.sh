568bfd014ec2c5b546aa55e1715cc14c95bce08d
msnape90 <msnape90@gmail.com>
2026-03-25 15:39:40 -0400

    added local bin and keybind to bashrc

--- tmux.sh
+++ tmux.sh
@@ -35,13 +35,16 @@ SYS_FILE_RELOAD_TMUX_CONF="$SYS_DIR_TMUX/reload-tmux.conf"
 SYS_FILE_TPM_INSTALL_PLUGINS="$SYS_DIR_TPM/bin/install_plugins"
 SYS_FILE_SESSIONIZER_BIN="$SYS_LOCAL_BIN/tmux-sessionizer"
 
+# create any needed dirs
 rm -rf "$SYS_DIR_TMUX"
 mkdir -p "$SYS_DIR_TMUX"
 mkdir -p "$SYS_RESURRECT_DIR"
 mkdir -p "$SYS_LOCAL_BIN"
 
+# install required packages
 sudo apt update && sudo apt install fzf tmux git -y
 
+# download requied git repos
 if [ ! -d "$SYS_DIR_TPM" ]; then
   git clone https://github.com/tmux-plugins/tpm "$SYS_DIR_TPM"
 fi
@@ -50,13 +53,22 @@ if [ ! -d "$SYS_DIR_SESSIONIZER" ]; then
   git clone https://github.com/ThePrimeagen/tmux-sessionizer "$SYS_DIR_SESSIONIZER"
 fi
 
+# copy the config and execution files
 cp "$SCRIPT_FILE_TMUX_CONF" "$SYS_FILE_TMUX_CONF"
 cp "$SCRIPT_FILE_RELOAD_TMUX_CONF" "$SYS_FILE_RELOAD_TMUX_CONF"
 cp "$SYS_DIR_SESSIONIZER/tmux-sessionizer" "$SYS_FILE_SESSIONIZER_BIN"
 
+# loads tmux and installs plugins
 tmux -f "$SYS_FILE_TMUX_CONF" new-session -d -s bootstrap
 tmux send-keys 'sleep 1' C-m &&
   sleep 1
 tmux -f "$SYS_FILE_TMUX_CONF" run-shell "$SYS_FILE_TPM_INSTALL_PLUGINS" &&
   sleep 3
 tmux kill-session -t bootstrap
+
+# creates the required keybindings and paths
+LINETOADD='bind -x '"\C-w": "who"''
+grep -qxF "$LINETOADD" "$HOME/.bashrc" || echo "$LINETOADD" >>"$HOME/.bashrc"
+
+LINETOADD='export PATH="$PATH:$HOME/.local/bin"'
+grep -qxF "$LINETOADD" "$HOME/.bashrc" || echo "$LINETOADD" >>"$HOME/.bashrc"
